3b63af661c0fb714ef9e7f95f74da86324875ced
ghiberti85 <ghiberti85@gmail.com>
2024-01-10 22:58:27 -0300

    Styling 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Link from "next/link";
+import { NotFoundContainer } from "@/styles/pages/404";
+
+
+export default function NotFound() {
+    return(
+        <>
+            <Head>
+                <title>Página não encontrada | Ignite Shop</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="robots" content="noindex" />
+            </Head>
+
+            <NotFoundContainer>
+                <h1>PÁGINA NÃO ENCONTRADA</h1>
+
+                <Link href="/">Voltar ao catálogo</Link>
+            </NotFoundContainer>
+        </>
+    )
+}
