4ea616aeb69d4a55f8911d7457d2385ebe5475b7
ghiberti85 <ghiberti85@gmail.com>
2024-01-12 20:24:42 -0300

    Button Component Style

---
+++ src/styles/components/button.ts
@@ -0,0 +1,23 @@
+import { styled } from "..";
+
+
+export const Button = styled('button', {
+    marginTop: 'auto',
+    padding: '1.25rem',
+    background: '$green500',
+    border: 0,
+    borderRadius: 8,
+    color: '$white',
+    fontSize: '$md',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+
+    '&:disabled': {
+        opacity: 0.6,
+        cursor: 'not-allowed',
+    },
+
+    '&:not(:disabled):hover': {
+        background: '$green300',
+    },
+})
